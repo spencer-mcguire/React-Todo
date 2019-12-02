c6f336775ffd32a55f80461067a337fe360ce8d1
spencer-mcguire <smcguire1826@gmail.com>
2019-12-02 15:25:52 -0600

    add new task functionality

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import TodoForm from "./components/TodoComponents/TodoForm";
 import TodoList from "./components/TodoComponents/TodoList";
 
 const data = [
-  {
-    task: "Organize Garage",
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: "Bake Cookies",
-    id: 1528817084358,
-    completed: false
-  }
+  // {
+  //   task: "Organize Garage",
+  //   id: 1528817077286,
+  //   completed: false
+  // },
+  // {
+  //   task: "Bake Cookies",
+  //   id: 1528817084358,
+  //   completed: false
+  // }
 ];
 
 class App extends React.Component {
@@ -25,12 +25,24 @@ class App extends React.Component {
   }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  addNewTask = task => {
+    const newTask = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      data: [...this.state.data, newTask]
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoForm />
-        <TodoList data={data} />
+        <TodoForm addNewTask={this.addNewTask} />
+        <TodoList data={this.state.data} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,10 +1,36 @@
 import React from "react";
 
 class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      newTask: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      newTask: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addNewTask(this.state.newTask);
+    this.setState({
+      newTask: ""
+    });
+  };
+
   render() {
     return (
-      <form>
-        <input />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          value={this.state.newTask}
+          onChange={this.handleChange}
+        />
         <button>Add Todo</button>
         <button>Clear Completed</button>
       </form>
